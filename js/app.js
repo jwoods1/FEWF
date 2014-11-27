c948db26a702ffdf707fee264d4ec219c05cfd82
b87775 <jwoods1@fullsail.edu>
2014-11-26 17:20:21 -0700

    added item list with add and remove.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,22 @@
 // Main js
 var app= angular.module('groceryList',[]);
+
+
+app.controller("listCtrl", function($scope){
+	$scope.items = [
+		{'name':'Bananas','done':false},
+		{'name':'Bread','done':false},
+		{'name':'Coffee','done':false},
+		{'name':'Creamer','done':false}
+
+	];
+
+	$scope.addItem = function(){
+		$scope.items.push({name:$scope.formItemText, done:false});
+		$scope.formItemText="";
+	};
+	$scope.removeItem = function(index){
+    	$scope.items.splice(index, 1);
+  	}
+
+});
